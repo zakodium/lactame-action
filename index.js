7964bc8b64c04330c4478b77b402a84de87ae668
Luc Patiny <lpatiny@users.noreply.github.com>
2021-04-24 19:21:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const folder = core.getInput('folder') || 'dist';
     const body = await got.post(`https://direct.lactame.com/lib/upload.php`, {
       body: form,
     });
-    core.info(body);
+    core.info(Object.keys(body));
   } catch (error) {
     return core.setFailed(
       `Post error (${error.response.statusCode} - ${error.response.statusMessage}): ${error.response.body}`,
