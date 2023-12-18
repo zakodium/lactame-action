d8e0cf4383a8a6d4b32ee9d31e9ebef57026117f
Michaël Zasso <targos@protonmail.com>
2023-12-18 11:05:38 +0100

    feat: update dependencies

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-'use strict';
+import { createReadStream, createWriteStream } from 'node:fs';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { pipeline } from 'node:stream/promises';
 
-const { createReadStream, createWriteStream } = require('fs');
-const fs = require('fs').promises;
-const path = require('path');
-const pipeline = require('util').promisify(require('stream').pipeline);
-
-const core = require('@actions/core');
-const archiver = require('archiver');
-const got = require('got');
-const FormData = require('form-data');
+import core from '@actions/core';
+import archiver from 'archiver';
+import got from 'got';
+import FormData from 'form-data';
 
 const token = core.getInput('token', { required: true });
 const folder = core.getInput('folder') || 'dist';
 
-(async () => {
-  await checkFolder();
-  const packageJson = await getPackageJson();
-  const version = core.getInput('version') || packageJson.version;
-  const name = core.getInput('name') || packageJson.name;
-  core.info('Package name: '+name);
-  
-  // Create .zip archive to send
-  const releaseDir = await fs.mkdtemp('lactame-release-');
-  const releaseZip = `${releaseDir}.zip`;
-  await fs.rename(folder, path.join(releaseDir, version));
-  const archive = archiver('zip', { zlib: { level: 9 } });
-  archive.directory(releaseDir + '/', name);
-  archive.finalize();
-  await pipeline(archive, createWriteStream(releaseZip));
+await checkFolder();
+const packageJson = await getPackageJson();
+const version = core.getInput('version') || packageJson.version;
+const name = core.getInput('name') || packageJson.name;
+core.info('Package name: ' + name);
 
-  // Send .zip archive
-  const form = new FormData();
-  form.append('upfile', createReadStream(releaseZip), {
-    filename: `${releaseDir}.zip`,
-    contentType: 'application/zip',
+// Create .zip archive to send
+const releaseDir = await fs.mkdtemp('lactame-release-');
+const releaseZip = `${releaseDir}.zip`;
+await fs.rename(folder, path.join(releaseDir, version));
+const archive = archiver('zip', { zlib: { level: 9 } });
+archive.directory(releaseDir + '/', name);
+archive.finalize();
+await pipeline(archive, createWriteStream(releaseZip));
+
+// Send .zip archive
+const form = new FormData();
+form.append('upfile', createReadStream(releaseZip), {
+  filename: `${releaseDir}.zip`,
+  contentType: 'application/zip',
+});
+form.append('token', token);
+try {
+  await got.post(`https://direct.lactame.com/lib/upload.php`, {
+    body: form,
   });
-  form.append('token', token);
-  try {
-    await got.post(`https://direct.lactame.com/lib/upload.php`, {
-      body: form,
-    });
-  } catch (error) {
-    return core.setFailed(
-      `Post error (${error.response.statusCode} - ${error.response.statusMessage}): ${error.response.body}`,
-    );
-  }
 
   core.info(
     `Release published to https://www.lactame.com/lib/${name}/${version}/`,
   );
-})().catch((error) => {
-  core.setFailed(error);
-});
+} catch (error) {
+  core.setFailed(
+    `Post error (${error.response.statusCode} - ${error.response.statusMessage}): ${error.response.body}`,
+  );
+}
 
 async function checkFolder() {
   try {
